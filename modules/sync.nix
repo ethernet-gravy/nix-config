a202166fe77099a11a99a3559f95d49dd12bc53a
Nakul <nakulsodhi@protonmail.com>
2025-03-09 16:00:57 -0500

    fixed the sync module

--- modules/sync.nix
+++ modules/sync.nix
@@ -8,9 +8,10 @@
             configDir = "/home/nakul/.config/syncthing";   # Folder for Syncthing's settings and keys
         };
     };
-}
+
    networking.firewall.allowedTCPPorts = [ 8384 22000 ];
    networking.firewall.allowedUDPPorts = [ 22000 21027 ];
    environment.systemPackages = with pkgs; [
             stc-cli
-   ]
+   ];
+}
