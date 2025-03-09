d539a489189dd4b96956498a5adea9e220d9bed2
Nakul Sodhi <nakulsodhi@protonmail.com>
2025-03-09 18:11:09 -0500

    make the server truly headless

--- server/configuration.nix
+++ server/configuration.nix
@@ -35,8 +35,8 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  #services.displayManager.sddm.enable = true;
+  #services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -91,7 +91,7 @@ services.openssh = {
 
 
   # Install firefox.
-  programs.firefox.enable = true;
+  #programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
