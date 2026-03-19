abde4dece3f109358f8493413781e7e5c94f9adb
Nakul S. <nakulsodhi@protonmail.com>
2026-03-19 18:25:29 -0500

    added udev rules for plugdev/rp2040 for now

--- modules/developement.nix
+++ modules/developement.nix
@@ -36,6 +36,35 @@ programs.neovim = {
     enable = true;
     defaultEditor = true;
 };
+
+
+# added udev rules to allow programming the rp2040 for now.
+services.udev.extraRules = ''
+SUBSYSTEM=="usb", \
+    ATTRS{idVendor}=="2e8a", \
+    ATTRS{idProduct}=="0003", \
+    TAG+="uaccess", \
+    MODE="660", \
+    GROUP="plugdev"
+SUBSYSTEM=="usb", \
+    ATTRS{idVendor}=="2e8a", \
+    ATTRS{idProduct}=="0009", \
+    TAG+="uaccess", \
+    MODE="660", \
+    GROUP="plugdev"
+SUBSYSTEM=="usb", \
+    ATTRS{idVendor}=="2e8a", \
+    ATTRS{idProduct}=="000a", \
+    TAG+="uaccess", \
+    MODE="660", \
+    GROUP="plugdev"
+SUBSYSTEM=="usb", \
+    ATTRS{idVendor}=="2e8a", \
+    ATTRS{idProduct}=="000f", \
+    TAG+="uaccess", \
+    MODE="660", \
+    GROUP="plugdev"
+'';
     
     services.emacs = {
         enable = false;
